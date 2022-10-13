1ef30a77d186d726b60cd6390343f0d970513b24
Emanuel Zini <emanuel.zini@8baselabs.com>
2022-10-13 11:54:46 -0300

    github actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-
   e2e: {
-    baseUrl: 'https://www.myshop4.com/',
-     
+    baseUrl: "https://www.myshop4.com/",
+    projectId: "3zu4x9",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
